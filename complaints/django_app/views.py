499b60763d534427bd2c2e6e1801aa589fe14a14
alemaiglikova <alemashelly123@gmail.com>
2023-08-19 14:57:26 +0600

    Django ORM.

---
+++ complaints/django_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
+
+def feedback_view(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('feedback_success')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback/feedback_form.html', {'form': form})
+
+def feedback_success_view(request):
+    return render(request, 'feedback/feedback_success.html')
